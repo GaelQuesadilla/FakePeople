8b3ec74f31d2e35f41fb25aa4271321493d80859
Gael González <gaeldev032@gmail.com>
2024-05-06 07:43:14 -0700

    changes

--- models.py
+++ models.py
@@ -14,10 +14,8 @@ characters = "ABCDEFGHIJKLNÑMNOPQRSTUVWXYZ1234567890"
 def dateBetweenYears(date, minYears, maxYears):
     minDays = minYears * 365.25
     maxDays = maxYears * 365.25
-
     daysSinceDate = random.randrange(round(minDays), round(maxDays))
     newDate = date + datetime.timedelta(days=daysSinceDate)
-
     return newDate
 
 
@@ -31,7 +29,6 @@ def genKey(chars, n):
     key = ""
     for _ in range(0, n):
         key += random.choices(chars)[0]
-
     return key
 
 
@@ -74,6 +71,64 @@ class Persons:
             item["birthState"] = random.choice(availableStates)[0]
         print(Fore.GREEN + f"Birthday state has ben added to {mods} items")
 
+    def addNames(self, attempts=5, unique=True):
+        mods = 0
+        for item in self.items:
+            if item.get("name") is not None:
+                continue
+            for attempt in range(attempts):
+                try:
+                    availableLastNames = json.load(
+                        open("./names/lastNames.json", "r", encoding="UTF-8"))
+
+                    if item.get("gender") is None:
+                        raise errors.ItemNeedHaveGenreProperty
+                    if item.get("gender") == "male":
+                        availableNames = json.load(
+                            open("./names/maleNames.json", "r", encoding="UTF-8"))
+                    elif item.get("gender") == "female":
+                        availableNames = json.load(
+                            open("./names/femaleNames.json", "r", encoding="UTF-8"))
+
+                    name = random.choice(availableNames)
+                    hasSecondName = random.choice([True, False])
+                    if hasSecondName:
+                        secondName = random.choice(availableNames)
+                        name += f" {secondName}"
+                    fLastName = random.choice(availableLastNames)
+                    mLastName = random.choice(availableLastNames)
+                    newName = {
+                        "name": name,
+                        "fLastName": fLastName,
+                        "mLastName": mLastName
+                    }
+                    if self.genFullNameString(newName) in [self.genFullNameString(item) for item in self.items]:
+                        raise errors.ItemPropertyAlreadyExist
+                        continue
+                    item.update(newName)
+                    mods += 1
+                    break
+                except errors.ItemPropertyAlreadyExist:
+                    print(Fore.YELLOW + f"NAME ALREADY EXIST, ATTEMPT {
+                        attempt+1} OF {attempts}")
+                    if attempt+1 == attempts:
+                        print("ERROR, CAN NOT GENERATE A UNIQUE NAME")
+                        break
+                except errors.ItemNeedHaveGenreProperty:
+                    break
+        print(Fore.GREEN + f"Name has ben added to {mods} items")
+
+    def genFullNameString(self, item):
+        return f"{item.get("fLastName")} {item.get("mLastName")} {item.get("name")}"
+
+    def saveAsCsv(self, docName="data.csv"):
+        with open(docName, "w", encoding="UTF-8") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=self.items[0].keys())
+            writer.writeheader()
+            writer.writerows(self.items)
+
+
+class Mexican(Persons):
     def addRFC(self):
         mods = 0
         for item in self.items:
@@ -139,65 +194,6 @@ class Persons:
             )
             item["NSS"] = NSS
 
-    def addNames(self, attempts=5, unique=True):
-        mods = 0
-        for item in self.items:
-            if item.get("name") is not None:
-                continue
-            for attempt in range(attempts):
-                try:
-                    availableLastNames = json.load(
-                        open("./names/lastNames.json", "r", encoding="UTF-8"))
-
-                    if item.get("gender") is None:
-                        raise errors.ItemNeedHaveGenreProperty
-
-                    if item.get("gender") == "male":
-                        availableNames = json.load(
-                            open("./names/maleNames.json", "r", encoding="UTF-8"))
-                    elif item.get("gender") == "female":
-                        availableNames = json.load(
-                            open("./names/femaleNames.json", "r", encoding="UTF-8"))
-
-                    name = random.choice(availableNames)
-                    hasSecondName = random.choice([True, False])
-                    if hasSecondName:
-                        secondName = random.choice(availableNames)
-                        name += f" {secondName}"
-
-                    fLastName = random.choice(availableLastNames)
-                    mLastName = random.choice(availableLastNames)
-                    newName = {
-                        "name": name,
-                        "fLastName": fLastName,
-                        "mLastName": mLastName
-                    }
-                    if self.genFullNameString(newName) in [self.genFullNameString(item) for item in self.items]:
-                        raise errors.ItemPropertyAlreadyExist
-                        continue
-
-                    item.update(newName)
-                    mods += 1
-                    break
-                except errors.ItemPropertyAlreadyExist:
-                    print(Fore.YELLOW + f"NAME ALREADY EXIST, ATTEMPT {
-                        attempt+1} OF {attempts}")
-                    if attempt+1 == attempts:
-                        print("ERROR, CAN NOT GENERATE A UNIQUE NAME")
-                        break
-                except errors.ItemNeedHaveGenreProperty:
-                    break
-        print(Fore.GREEN + f"Name has ben added to {mods} items")
-
-    def genFullNameString(self, item):
-        return f"{item.get("fLastName")} {item.get("mLastName")} {item.get("name")}"
-
-    def saveAsCsv(self, docName="data.csv"):
-        with open(docName, "w", encoding="UTF-8") as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=self.items[0].keys())
-            writer.writeheader()
-            writer.writerows(self.items)
-
 
 class Students(Persons):
     def addGrade(self, number, grade, **kwargs):
@@ -211,7 +207,6 @@ class Students(Persons):
                 mods += 1
             if mods == number:
                 break
-
         if mods == number:
             print(
                 Fore.GREEN + f"grade has ben added to {mods} of {number} items")
@@ -224,13 +219,11 @@ class Students(Persons):
         for item in self.items:
             if item.get("score") is None:
                 idealScore = round(random.gauss(mu, sigma), floatNumbers)
-
                 score = idealScore
                 if idealScore > maxScore:
                     score = maxScore
                 elif idealScore < minimalScore:
                     score = minimalScore
-
                 item["score"] = score
                 mods += 1
         print(Fore.GREEN + f"score has ben added to {mods} items")
@@ -243,7 +236,6 @@ class Students(Persons):
             for option in options:
                 item[option] = selections.pop()
                 mods += 1
-
         print(Fore.GREEN + f"selections has ben added to {mods} items")
 
 
@@ -269,13 +261,14 @@ class Students(Persons):
 # students.saveAsCsv()
 
 
-persons = Persons()
-persons.create(500)
-persons.addGenre()
-persons.addNames()
-persons.addBirthDate()
-persons.addRFC()
-persons.addBirthState()
-persons.addCURP()
-persons.addNSS()
-persons.saveAsCsv()
+if __name__ == "__main__":
+    persons = Mexican()
+    persons.create(500)
+    persons.addGenre()
+    persons.addNames()
+    persons.addBirthDate()
+    persons.addRFC()
+    persons.addBirthState()
+    persons.addCURP()
+    persons.addNSS()
+    persons.saveAsCsv()
